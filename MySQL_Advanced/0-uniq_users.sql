ec9873546ef5eee35d057c239993796d8cacbb1d
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-06-08 16:22:30 +0400

    mysql_advanced editing

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,12 @@
--- Script that creates a table users following these requirements:
--- id, email and name
-CREATE TABLE IF NOT EXISTS users(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If table exists, script will not fail, can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
